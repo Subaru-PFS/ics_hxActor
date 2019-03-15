4155e74427519da0f7561dc928372179c9ebe82d
Craig Loomis <cloomis@astro.princeton.edu>
2019-03-15 10:30:51 -0400

    Replace command line camera name override with a config tweak
    Used --imageCamName to override teh camera name used for image
    files. Change this to a `imageCamName` config tweak.
    
    Note that this will only be used for lab bench testing, probably only
    at JHU.

--- python/hxActor/main.py
+++ python/hxActor/main.py
@@ -14,43 +14,49 @@ class OurActor(actorcore.ICC.ICC):
     def __init__(self, name,
                  productName=None,
                  camName=None,
-                 imageCamName=None,
                  debugLevel=30):
 
         """ Setup an Actor instance. See help for actorcore.Actor for details. """
         
         self.instrument = instrument
         if instrument == 'PFS':
-            if imageCamName is None:
-                imageCamName = camName
-            self.spectroIds = spectroIds.SpectroIds(partName=imageCamName)
-            if camName is None:
-                camName = self.spectroIds.camName
-            specName = self.spectroIds.specName
-            name = f"hx_{camName}"
-            modelNames = ("hx_{camName}", "xcu_{camName}", "enu_{specName}")
+            self.ids = spectroIds.SpectroIds(partName=camName)
+            if name is None:
+                name = f"hx_{self.ids.camName}"
+            modelNames = ()
         else:
             name = "hx"
             modelNames = ('hx',)
             
         # This sets up the connections to/from the hub, the logger, and the twisted reactor.
         #
-        print(f'configuring for {name}, with models={modelNames}, instrument={instrument}')
+        print(f'configuring for {name}, instrument={instrument}')
         actorcore.ICC.ICC.__init__(self, name, 
                                    productName=productName,
                                    modelNames=modelNames)
+        try:
+            imageCamName = self.config.get(self.name, 'imageCamName')
+            self.ids = spectroIds.SpectroIds(partName=imageCamName)
+            self.logger.warning(f'RECONFIGURED for imageCam {imageCamName}')
+        except Exception:
+            pass
 
         self.everConnected = False
 
     def connectionMade(self):
         if self.everConnected is False:
-            self.logger.info(f'ids: {self.spectroIds.idDict}')
+            self.logger.info(f'{self.name} ids: {self.ids.idDict}')
             self.logger.info("Attaching all controllers...")
             self.allControllers = [s.strip() for s in self.config.get(self.name, 'startingControllers').split(',')]
             self.attachAllControllers()
             self.everConnected = True
-        
 
+            models = [m % self.ids.idDict for m in ('xcu_%(camName)s', 'hx_%(camName)s')]
+#                                                    'enu_%(specName)s', 'dcb_%(specName)s',)]
+            self.logger.info('adding models: %s', models)
+            self.addModels(models)
+            self.logger.info('added models: %s', self.models.keys())
+            
 #
 # To work
 def main():
@@ -61,14 +67,11 @@ def main():
                         help='logging level')
     parser.add_argument('--cam', default=None, nargs='?',
                         help='camera name')
-    parser.add_argument('--imageCam', default=None, nargs='?',
-                        help='camera name for image files')
     args = parser.parse_args()
     
     theActor = OurActor(None,
                         productName='hxActor',
-                        camName=args.cam,
-                        imageCamName=args.imageCam)
+                        camName=args.cam)
     theActor.run()
 
 if __name__ == '__main__':
